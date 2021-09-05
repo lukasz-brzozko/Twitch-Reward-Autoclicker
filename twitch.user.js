f539f9465fefb1f4f3a8fd6e4b5ec9901fcc7913
Lukasz Brzozko <lukasz9099@gmail.com>
2021-09-05 11:59:15 +0200

    feat: add meta and user files

--- twitch.user.js
+++ twitch.user.js
@@ -1,7 +1,12 @@
 // ==UserScript==
 // @name         Twitch Reward Autoclicker
+// @namespace    https://github.com/lukasz-brzozko/Twitch-Reward-Autoclicker
 // @version      0.1.4
+// @description  Twitch Reward Autoclicker
+// @author       Łukasz Brzózko
 // @include      https://www.twitch.tv/*
+// @updateURL    https://raw.githubusercontent.com/lukasz-brzozko/steam-queue-autoclicker/master/autoclicker.meta.js
+// @downloadURL  https://raw.githubusercontent.com/lukasz-brzozko/steam-queue-autoclicker/master/autoclicker.user.js
 // @grant        none
 // ==/UserScript==
 
