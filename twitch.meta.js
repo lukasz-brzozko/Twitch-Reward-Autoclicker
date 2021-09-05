4d8bd97d78022d6c83bd404db13f0bcd6b6568f8
Lukasz Brzozko <lukasz9099@gmail.com>
2021-09-05 12:07:57 +0200

    fix: update links

--- twitch.meta.js
+++ twitch.meta.js
@@ -5,7 +5,7 @@
 // @description  Twitch Reward Autoclicker
 // @author       Łukasz Brzózko
 // @include      https://www.twitch.tv/*
-// @updateURL    https://raw.githubusercontent.com/lukasz-brzozko/steam-queue-autoclicker/master/autoclicker.meta.js
-// @downloadURL  https://raw.githubusercontent.com/lukasz-brzozko/steam-queue-autoclicker/master/autoclicker.user.js
+// @updateURL    https://raw.githubusercontent.com/lukasz-brzozko/Twitch-Reward-Autoclicker/main/twitch.meta.js
+// @downloadURL  https://raw.githubusercontent.com/lukasz-brzozko/Twitch-Reward-Autoclicker/main/twitch.user.js
 // @grant        none
 // ==/UserScript==
