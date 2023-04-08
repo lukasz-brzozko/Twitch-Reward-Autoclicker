1c3bce9864f60d0639b409ae3cbf0287d6e85ac0
Łukasz Brzózko <lukasz9099@gmail.com>
2023-04-08 22:15:31 +0200

    fix: indent

--- twitch.meta.js
+++ twitch.meta.js
@@ -4,7 +4,7 @@
 // @version      0.2.2
 // @description  Twitch Reward Autoclicker
 // @author       Łukasz Brzózko
-// @match      https://www.twitch.tv/*
+// @match        https://www.twitch.tv/*
 // @updateURL    https://raw.githubusercontent.com/lukasz-brzozko/Twitch-Reward-Autoclicker/main/twitch.meta.js
 // @downloadURL  https://raw.githubusercontent.com/lukasz-brzozko/Twitch-Reward-Autoclicker/main/twitch.user.js
 // @grant        none
