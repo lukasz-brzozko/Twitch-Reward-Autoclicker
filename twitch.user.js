a807d9e0569ca309e329ac4ceb6ebd1376a84661
Łukasz Brzózko <lukasz9099@gmail.com>
2023-05-04 13:17:34 +0200

    feat: add script icon

--- twitch.user.js
+++ twitch.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Twitch Reward Autoclicker
 // @namespace    https://github.com/lukasz-brzozko/Twitch-Reward-Autoclicker
-// @version      0.2.2
+// @version      0.2.3
 // @description  Twitch Reward Autoclicker
 // @author       Łukasz Brzózko
 // @match        https://www.twitch.tv/*
+// @icon         https://static.twitchcdn.net/assets/favicon-32-e29e246c157142c94346.png
 // @updateURL    https://raw.githubusercontent.com/lukasz-brzozko/Twitch-Reward-Autoclicker/main/twitch.meta.js
 // @downloadURL  https://raw.githubusercontent.com/lukasz-brzozko/Twitch-Reward-Autoclicker/main/twitch.user.js
 // @grant        none
